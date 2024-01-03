1d5b145e401d64b39bcf92c24f64cc79c82e7bc0
ulaganathan <ulaganathan@amshuhu.com>
2024-01-03 17:09:46 +0530

    changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -5,7 +5,7 @@ const corsOptions = {
     //Gitlab frontend registry url - https://myapps-frontend-container.onrender.com
 
     
-    origin: ['http://localhost:4200', 'https://myapps-jbmx.onrender.com', 'https://myapps-frontend-container.onrender.com']
+    origin: ['http://localhost:4200', 'https://myapps-jbmx.onrender.com']
   };
 
 
